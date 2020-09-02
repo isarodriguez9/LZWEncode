f980f8dae2abba23222145c0e3d9d4f30c3ac422
QPC1234 <quinncallaghan@gmail.com>
2020-09-02 12:56:12 -0700

    StarterTable

--- src/Encoder.java
+++ src/Encoder.java
@@ -6,5 +6,9 @@ public class Encoder {
 	
 	public void Encode(String FileName) throws FileNotFoundException {
 		BufferedReader br = new BufferedReader(new FileReader(FileName));
+		ArrayList<String> encodingTable = new ArrayList<String>();
+		for (int i = 0; i < 255; i++) {
+			encodingTable.add((char)i + "");
+		}
 	}
 }
