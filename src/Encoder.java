f03941cf5ee54a22044bc6122983be6c391cf733
QPC1234 <quinncallaghan@gmail.com>
2020-09-02 13:12:27 -0700

    Update Encoder.java

--- src/Encoder.java
+++ src/Encoder.java
@@ -2,10 +2,10 @@ import java.util.*;
 import java.io.*;
 
 public class Encoder {
-	private HashMap<String, Integer> encodingTable;
+	private HashMap<String, Integer> encodingTable; //Stores all encodings in a HashMap
 	
 	/**
-	 * 
+	 * Fills the Hashmap with all 255 single chars.
 	 */
 	public Encoder() {
 		encodingTable = new HashMap<String, Integer>();
