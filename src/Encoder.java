19d5930fa8c6d5b1f19b641ef289398a1254fc8a
QPC1234 <quinncallaghan@gmail.com>
2020-09-02 12:58:27 -0700

    constructor 2 electric boogaloo

--- src/Encoder.java
+++ src/Encoder.java
@@ -2,13 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class Encoder {
-	public Encoder() {}
+	private ArrayList<String> encodingTable;
 	
-	public void Encode(String FileName) throws FileNotFoundException {
-		BufferedReader br = new BufferedReader(new FileReader(FileName));
-		ArrayList<String> encodingTable = new ArrayList<String>();
+	public Encoder() {
+		encodingTable = new ArrayList<String>();
 		for (int i = 0; i < 255; i++) {
 			encodingTable.add((char)i + "");
 		}
+	}
+	
+	public void Encode(String FileName) throws FileNotFoundException {
+		BufferedReader br = new BufferedReader(new FileReader(FileName));
 	}
 }
