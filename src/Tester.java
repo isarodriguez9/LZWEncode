0e562a25ea67df3df50483e954d450c1e699f1d0
QPC1234 <quinncallaghan@gmail.com>
2020-09-04 10:34:51 -0700

    h

--- src/Tester.java
+++ src/Tester.java
@@ -5,10 +5,5 @@ public class Tester {
 	public static void main(String[] args) throws IOException {
 		Encoder encoder = new Encoder();
 		encoder.Encode("encoding1", "encoded");
-		BufferedReader br = new BufferedReader(new FileReader("encoded"));
-		while (br.ready()) {
-			System.out.print(br.read());
-		}
-		br.close();
 	}
 }
