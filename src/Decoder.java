73f9a3ed8af8c0c3c03cf85664e249590c892511
woofasaurus <dinocove@gmail.com>
2020-09-18 08:53:38 -0700

    decoder now consistent with encoder

--- src/Decoder.java
+++ src/Decoder.java
@@ -1,5 +1,18 @@
+import java.util.*;
+import java.io.*;
+
 public class Decoder {
-	public Decoder () {
-		
+
+	private HashMap<String, Integer> encodingTable; //Stores all encodings in a HashMap
+
+	public Decoder () { //copy-pasted setup from Encoder
+		encodingTable = new HashMap<String, Integer>();
+		for (int i = 0; i <= 255; i++) {
+			encodingTable.put((char)i + "", i);
+		}
+	}
+
+	public void Decode() {
+
 	}
 }
