1822226a2c0a7aba47097d7a07e4fbb48904001d
woofasaurus <dinocove@gmail.com>
2020-09-18 09:58:25 -0700

    Now sends the correct string to the write method and dynamically adds to hashmap

--- src/Decoder.java
+++ src/Decoder.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 public class Decoder {
 
-	private HashMap<String, Integer> encodingTable; //Stores all encodings in a HashMap
+	private HashMap<String, Integer> encodingTable; //Stores all encodings in a HashMap (could be a normal array but I'm lazy)
 
-	public Decoder () { //copy-pasted setup from Encoder
-		encodingTable = new HashMap<String, Integer>();
+	public Decoder () { //copy-pasted setup from Encoder but reversed to int,string
+		encodingTable = new HashMap<Integer,String>();
 		for (int i = 0; i <= 255; i++) {
-			encodingTable.put((char)i + "", i);
+			encodingTable.put(i, (char)i + "");
 		}
 	}
 
@@ -23,11 +23,30 @@ public class Decoder {
 			
 			int nextEncoding = 256;//keeps track of the next set of chars' code number in the hashmap
 
+			Integer prevValue = "";
+			Integer code = 0;
 			String next = (char)br.read() + "";//next set of characters
 			String read = next; //stores immediate read output
+
 			while (br.ready()) {
 				while ((read = br.read) != (char)" ") { //Checks if br.read is a space, if not adds it to the total integer.
 					next+=(char)br.read();
+				}
+
+				code = Integer.ValueOf(next);
+
+				if (encodingTable.containsKey(code)) {
+					write(encodingTable.get(code));
+				} else {
+					encodingTable.put(nextEncoding, prevValue + prevValue.substring(0,1)); //Adds edge case to HashMap (there is a way to make this not necessary and to only add to encoding table in one step always but i dont wanna rn)
+					write(encodingTable.get(code));
+					nextEncoding++; 
+				}
+				
+				String check = prevValue + encodingTable.get(code).substring(0,1); // stores previous iteration + first letter of new iteration 
+				if (encodingTable.get(nextEncoding-1).equals(check)) {             // if it isnt already in the table...
+					encodingTable.put(nextEncoding, check); //Adds it to HashMap
+					nextEncoding++;
 				}
 
 				/*
@@ -44,7 +63,7 @@ public class Decoder {
 	}
 	}
 
-	public void Write(){
+	public void Write(String value){
 
 	}
 }
