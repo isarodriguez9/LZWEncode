be2cfe9744cf7d32b79eeb299e5e552f4b256c45
isarodriguez9 <70167519+isarodriguez9@users.noreply.github.com>
2020-09-24 10:40:25 -0700

    fixing installing issues

--- src/Decoder.java
+++ src/Decoder.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-
 public class Decoder {
 
 	private HashMap<Integer, String> encodingTable; //Stores all encodings in a HashMap (could be a normal array but I'm lazy)
