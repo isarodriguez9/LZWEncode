5b5880ab0b1b4b0229e7871f3c95c8bf67dd1aa9
isarodriguez9 <70167519+isarodriguez9@users.noreply.github.com>
2020-09-29 09:17:40 -0700

    addHashValues method

--- src/Encoder.java
+++ src/Encoder.java
@@ -2,13 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Encoder {
+	final static int DICTINT= 128;
+	final static int MAXHASHSIZE = 5000;
 	private HashMap<String, Integer> encodingTable; //Stores all encodings in a HashMap
-	private myQueue queue = new myQueue(); 
+	private MyQueue queue = new MyQueue(); 
 	//Fills the Hashmap with all 255 single chars.
 	public Encoder() {
 		encodingTable = new HashMap<String, Integer>();
 		for (int i = 0; i <= 255; i++) {
 			encodingTable.put((char)i+"", i);
+		}
+	}
+	public void addHashValues()
+	{
+		for(int i = 0; i<DICTINT; i++)
+		{
+		//	queue.add(encodingTable.get(i).getStr());
 		}
 	}
 
