56ae418e0a106967ca867f7a52e54f4f569ef4e1
isarodriguez9 <70167519+isarodriguez9@users.noreply.github.com>
2020-09-29 23:37:34 -0700

    fixing overlap

--- src/Encoder.java
+++ src/Encoder.java
@@ -9,24 +9,11 @@ public class Encoder {
 	//Fills the Hashmap with all 255 single chars.
 	public Encoder() {
 		encodingTable = new HashMap<Node, Integer>();
-<<<<<<< Updated upstream
 		for (int i = 0; i < DICTINT; i++) {
 			Node toBeAdded = new Node((char)i+"",i);
 			encodingTable.put(toBeAdded,0);
 			queue.add(toBeAdded);
-=======
-		for (int i = 0; i <= 255; i++) {
-			encodingTable.put(new Node((char)i+"", i),0);
 		}
-	}
-	public void addHashValues()
-	{
-		for(int i = 0; i<DICTINT; i++)
-		{
-			queue.add(encodingTable.get(i).getStr());
->>>>>>> Stashed changes
-		}
-	}
 
 	public void Encode(String fileName) throws IOException {
 		try {
@@ -48,19 +35,11 @@ public class Encoder {
 				{
 					Node currentNode = new Node(next,nextEncoding);
 					encodingTable.put(currentNode,0); //Adds new code to Hashmap
-<<<<<<< Updated upstream
-					if(encodingTable.get(currentNode).equals(1))
-=======
 					if(encodingTable.get(currentNode)==1)
->>>>>>> Stashed changes
 					{
 						queue.remove(currentNode);
 					}
 					queue.add(currentNode);
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 					System.out.println("Next: " + next + " substring: " + next.substring(0, next.length()-1));
 					pw.print((char)encodingTable.get(next.substring(0, next.length()-1)).intValue());
 					System.out.println((int)'’');
@@ -69,11 +48,7 @@ public class Encoder {
 					if (encodingTable.containsKey(next)) {
 						pw.print((char)encodingTable.get(next).intValue());
 					} else {
-<<<<<<< Updated upstream
-						encodingTable.put(next, nextEncoding); //Adds new code to Hashmap
-=======
 						encodingTable.put(new Node(next, nextEncoding), 0); //Adds new code to Hashmap
->>>>>>> Stashed changes
 						pw.print((char)encodingTable.get(next.substring(0, next.length()-1)).intValue());
 						pw.print((char)encodingTable.get(next.substring(next.length()-1)).intValue());
 						nextEncoding++;
