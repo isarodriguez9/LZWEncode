9181a852f18482dcc27eaf7bb80e1a73646ef1b3
lily26bailey <lily26bailey@gmail.com>
2020-10-04 18:28:28 -0700

    comments

--- src/Encoder.java
+++ src/Encoder.java
@@ -28,8 +28,8 @@ public class Encoder {
 			int nextEncoding = DICTINT;//keeps track of the next set of chars' code number in the hashmap
 			String next = (char)br.read() + "";//next set of characters
 			while (br.ready()) {
-				Node containsKeyFalse = new Node(next, false);
-				Node containsKeyTrue = new Node(next, true);
+				Node containsKeyFalse = new Node(next, false); //creates a new node with the next set of chars and false if the sequence is not in the queue
+				Node containsKeyTrue = new Node(next, true); //creates a new node with the next set of chars and true in case the sequence is already in the queue
 				while(encodingTable.containsKey(containsKeyFalse)|| encodingTable.containsKey(containsKeyTrue)) { //Checks if next is in table, if it is, add next letter until it a new, previously unseen string is formed
 					next+=(char)br.read();
 					containsKeyFalse.setStr(next);
@@ -39,26 +39,26 @@ public class Encoder {
 				if (encodingTable.size()<MAXHASHSIZE)
 				{
 					Node currentNode = new Node(next,false);
-					Node toBeReplaced = new Node(next.substring(0, next.length()-1),false);
+					Node toBeReplaced = new Node(next.substring(0, next.length()-1),false); //creates a node to store the outprint and what will be added to the queue
 					encodingTable.put(currentNode,nextEncoding); //Adds new code to Hashmap
 					System.out.println("Next: " + next + " substring: " + next.substring(0, next.length()-1));
 					pw.print((char)encodingTable.get(toBeReplaced).intValue());
 					Node addToQueue = new Node(next.substring(0, next.length()-1), true);
 
-					if(encodingTable.containsKey(addToQueue))
+					if(encodingTable.containsKey(addToQueue)) //if the node is already in the queue, remove it before re-adding to the top 
 					{
 						queue.remove(addToQueue);
 					}
 					queue.add(addToQueue);
-
+					//replaces the old node with a new node with the same string, but boolean true since it is now in the queue
 					Integer replacedValue = encodingTable.get(toBeReplaced);
+					encodingTable.put(addToQueue,replacedValue); 
 					encodingTable.remove(toBeReplaced);
-					encodingTable.put(addToQueue,replacedValue);
 					System.out.println((int)'’');
 					nextEncoding++;
 				}
 
-				else
+				else //if the hashmap has reached its max size, remove the top node from the queue and remove from the hashmap
 				{
 					Node nodeToRemove = queue.removeFirst();
 					encodingTable.remove(nodeToRemove);
